59ea3cbe576a38ff716401dae183b996fc966da8
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 16:18:55 +0200

    Add print function for shift variable
    Added comments to clarify variable declarations and print statements.

---
+++ python-basics/print-shift-variable.py
@@ -0,0 +1,8 @@
+# Declare a string variable
+text = 'Hello World'
+# Print the data type of the 'text' variable
+print(type(text))
+# Declare an integer variable for the cipher shift
+shift = 3
+# Use the print() function to output the value of the 'shift' variable
+print(shift)
