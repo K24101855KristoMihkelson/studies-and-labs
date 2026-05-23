8fdafa1461e18cb68d79fb5d018df9b513689494
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 20:38:32 +0200

    Calculate shifted index for cipher logic
    Implement basic cipher to calculate shifted indices.

---
+++ python-basics/calculate_shift_index.py
@@ -0,0 +1,11 @@
+# Setup basic cipher variables
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# Iterate through the text, converting it to lowercase first
+for char in text.lower():
+    # Find the original index of the character in the alphabet
+    index = alphabet.find(char)
+    print(char, index)
+    # Calculate the new index by applying the shift value
+    new_index = index + shift
