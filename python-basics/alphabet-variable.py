8e6e00d9feab4eb6873a050852d327fa62c4a194
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 16:30:17 +0200

    Add alphabet string variable for cipher reference

---
+++ python-basics/alphabet-variable.py
@@ -0,0 +1,6 @@
+# Declare a string variable for the initial text
+text = 'Hello World'
+# Declare an integer for the shift amount
+shift = 3
+# Define the alphabet string to use as a reference for shifting characters
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
