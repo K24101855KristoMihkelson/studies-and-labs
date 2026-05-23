470e9b58e3e55cc183d48dcc07e05a296f1646d1
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 16:25:09 +0200

    Add data type check for shift variable
    Add a script to check and print data types of variables.

---
+++ python-basics/check-data-types.py
@@ -0,0 +1,8 @@
+# Declare a string variable for the text
+text = 'Hello World'
+# Output the data type of the 'text' variable (returns <class 'str'>)
+print(type(text))
+# Declare an integer variable for the cipher shift
+shift = 3
+# Output the data type of the 'shift' variable (returns <class 'int'>)
+print(type(shift))
