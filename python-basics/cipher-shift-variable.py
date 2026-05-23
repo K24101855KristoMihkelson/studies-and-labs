4423bf2fed61627fce553cdb36c2c1d70b7691fa
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 16:16:27 +0200

    Add shift variable for cipher logic

---
+++ python-basics/cipher-shift-variable.py
@@ -0,0 +1,6 @@
+# Declare a string variable for the initial message
+text = 'Hello World'
+# Verify and print the data type of the 'text' variable
+print(type(text))
+# Declare an integer variable to set the shift value for the cipher algorithm
+shift = 3
