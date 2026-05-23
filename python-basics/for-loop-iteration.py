80fc9684fef068ca5e3276db6bde92a40cab62e1
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 17:31:31 +0200

    Add for loop to iterate through text characters
    Set up basic variables for a cipher and initiate a for loop.

---
+++ python-basics/for-loop-iteration.py
@@ -0,0 +1,7 @@
+# Setup basic cipher variables
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# Initiate a for loop to iterate through each character in the text string
+# The variable 'i' represents the current character during each iteration step
+for i in text:
