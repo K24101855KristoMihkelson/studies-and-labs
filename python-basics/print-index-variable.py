854210173b0ff936d66d285e4fb635d0e4c74adc
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 17:00:39 +0200

    Add print function to verify stored index value
    Added code to find and print the index of the first character in the text.

---
+++ python-basics/print-index-variable.py
@@ -0,0 +1,8 @@
+# Define variables for the text, shift amount, and alphabet reference
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# Find the index of the first character ('H') and store it
+index = alphabet.find(text[0])
+# Print the index to the console to verify the result (outputs -1)
+print(index)
