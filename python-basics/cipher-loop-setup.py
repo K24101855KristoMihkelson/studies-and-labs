84adeed8c18158889ff47446302b0bed09bf6b20
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 17:29:43 +0200

    Clean up codebase to prepare for loop implementation
    Setup basic variables for the Caesar cipher and prepare for an automated loop implementation.

---
+++ python-basics/cipher-loop-setup.py
@@ -0,0 +1,5 @@
+# Setup basic variables for the Caesar cipher
+# The codebase is now prepared for implementing an automated loop
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
