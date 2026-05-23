5b9c538342433a5f957b4ba5f38e7f64cec7b916
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 15:33:41 +0200

    Add negative string indexing example
    Add example of negative indexing in a string.

---
+++ python-basics/string-negative-indexing.py
@@ -0,0 +1,5 @@
+# Declare a string variable
+text = 'Hello World'
+# Print the last character of the string using a negative index
+# Index -1 always refers to the very last character
+print(text[-1])
