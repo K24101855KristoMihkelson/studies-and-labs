74b6d6311f22bad551d8d541cb4079e3c07d76be
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 16:58:17 +0200

    Assign character find result to index variable
    Initialize variables for text, shift, and alphabet, and find the index of the first character.

---
+++ python-basics/character-index-variable.py
@@ -0,0 +1,7 @@
+# Define variables for text, shift, and alphabet
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# Find the index of the first character and store it in the 'index' variable
+# Storing the result allows us to use this number later in the calculation
+index = alphabet.find(text[0])
