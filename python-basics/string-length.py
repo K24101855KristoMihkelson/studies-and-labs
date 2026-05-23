057f12cd4f30f0c97e061d255061d08ae036faed
Kristo Mihkelson <krismihkel@gmail.com>
2026-05-23 16:03:31 +0200

    Add string length measurement using len function
    Add a script to calculate and print string length.

---
+++ python-basics/string-length.py
@@ -0,0 +1,5 @@
+# Declare a string variable
+text = 'Hello World'
+# Use the len() function to get the total number of characters and print it
+# Note: len() counts every single character, including spaces (outputs 11)
+print(len(text))
